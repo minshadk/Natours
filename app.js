4f45b2dc54c6b8c01262336c2b09abc810275178
minshadk <minshadk13@gmail.com>
2022-01-08 12:32:12 +0530

    comminting to chekout

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 
-const tourRouter = require("./routes/tourRouter");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -11,5 +15,22 @@ app.use(express.json());
 
 // ROUTES
 app.use("/api/v1/tours", tourRouter);
+// app.use("/api/v1/users", userRouter);
+
+// All the http request
+// app.all("*", (req, res, next) => {
+//   res.status(404).json({
+//     status: "fail",
+//     message: `Can't find ${req.originalUrl} on   this server`
+//   });
+
+// const err = new Error(`Can't find ${req.originalUrl} on   this server`);
+// err.status = "fail";
+// err.statusCode = 404;
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on   this server !`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
